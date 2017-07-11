9d6f4fe26489abf41e4c43f2333f169def191df1
Nadia <belozerovany@gmail.com>
2017-07-11 22:44:18 +0300

    wg progect

---
+++ app/js/common.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $('.full_slide').slick();
+  $('.main_slide').slick({
+  		/*autoplay:true,
+	    autoplaySpeed:3000,*/
+	    dots:true,
+  });
+
+	$('#myForm').validate({
+	  	rules: {
+	  		email: {
+	  			required:true,
+	  			email:true
+	  		},
+	  		name: {
+	  			required:true,
+	  			minlength:3
+	  		}
+	  	},
+	  	messages:{
+	  		email: {
+	  			required:'Поле email обязательное для заполнения',
+	  			email:'Пожалуйста введите корректный email'
+	  		},
+	  		name: {
+	  			required:'Имя обязательное для заполнения',
+	  			minlength:'Длина имени должно быть не менее 3-х символов'
+	  		}
+	  	}
+	});
+	$('.form_photo__form').validate({
+	  	rules: {
+	  		code: {
+	  			required:true,
+	  			minlength:3
+	  		}	
+	  	},
+	  	messages:{
+	  		code: {
+	  			required:' Поле обязательное для заполнения',
+	  			minlength:'Код  содержит не менее 3-х символов'
+	  		}
+	  	}
+	});
+});	 
